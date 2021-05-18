37b2c1bc821ec9e93754bbaffe6249373892517e
Agustin-Alice <agustin.alice19@gmail.com>
2021-05-18 17:06:51 -0300

    problems with the inputs

--- to-list/src/App.js
+++ to-list/src/App.js
@@ -1,22 +1,25 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react'
 import './App.css';
-
-const Inputs = (props) => {
-
-  let list = []
-  list.push(props.valores)
-  
-  
-  return(
-    <div >
-      <input type = 'text' value = {list}/>
-      <button>edit</button>
+ 
+const Inputs = (props) => {  
+  /*list.map((texto) => {
     
-    </div>
-    
-     
+  })*/
 
-  )
+  props.list.forEach((valor) => {
+    return(
+      <div >
+        <input type = 'text' value = {valor}/>
+        <button>edit</button>
+      
+      </div>
+      
+       
+  
+    )
+  })
+  
 }
 
 
@@ -52,9 +55,15 @@ class App extends Component {
   
   state = { 
     text: '',
-    cant_items: 0
+    list: []
   }
 
+  post = () => {
+    let text = this.text
+    this.state.list.push(text)
+    console.log(this.state.list)
+
+  }
 
 
   add = (event) => {
@@ -67,26 +76,18 @@ class App extends Component {
   // el valor del input del componente Inputs tiene que ser el valor del state del componente padre. 
   agregar = (event) => {
     event.preventDefault()
-    const cant_items = this.state.cant_items + 1
-    this.setState({cant_items}) 
-
-
+    this.post()
   }
 
   render() {
     return(
     
     <form className="App">
-      <section className="App-left">
-        {/*incluir condicion se renderiza el componente inputs la cantidad de click que tiene el state del componente app(componente padre).
-        El valor de cada input puede ser sacado del state del componente en un array 
-        
-        */}
-        
+       <section className="App-left">       
         <Inputs
-        valores= {this.state.text}
+        valores= {this.state.list}
         />
-      
+      <div>{this.state.list}</div>
       
       </section>
       
@@ -103,7 +104,6 @@ class App extends Component {
         </button>
 
         <h2>{this.state.text}</h2>
-        <h3>{this.state.cant_items}</h3>
       </section>
     </form>
   
